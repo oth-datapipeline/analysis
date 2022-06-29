44e25573f126391557aeab5f73247592927a15e5
tharealbasti <sebastian.strasser98@web.de>
2022-06-29 20:26:33 +0200

    Fix limit on analysis 'frequently used news sources' and adjust default limit on all analyses to 30

--- src/analyzers/reddit_analyzer.py
+++ src/analyzers/reddit_analyzer.py
@@ -21,9 +21,10 @@ class RedditAnalyzer:
         Analyzes how long comments are on average for each collected subreddit
         """
         if st.button('Show'):
-            result = pd.DataFrame(list(ap.reddit_comment_length_per_subreddit(self.collection))).sort_values(by='average_comment_length', ascending=True)
+            result = pd.DataFrame(list(ap.reddit_comment_length_per_subreddit(self.collection))).sort_values(by='average_comment_length', ascending=False)
             result.rename(columns={ 'subreddit': 'Subreddit', 'average_comment_length': 'Average comment length' }, inplace=True)
             fig = px.bar(result, x='Average comment length', y='Subreddit', orientation='h')
+            fig.update_yaxes(autorange='reversed')
             st.write(fig)
 
     def keyword_per_subreddit(self):
@@ -31,19 +32,20 @@ class RedditAnalyzer:
         Analyzes which keywords are commonly used on a specific subreddit
         """
         subreddit = st.selectbox(label='Subreddit', options=tuple(const.SUBREDDITS))
-        limit = int(st.text_input("Limit", value="100"))
+        limit = int(st.text_input("Limit", value="30"))
         output_wc = st.radio("Output as Wordcloud", options=tuple(["Yes", "No"]))
         if st.button('Show'):
             result = list(ap.reddit_keyword_per_subreddit(self.collection, subreddit))
             result = list(filter(lambda row: not row["keyword"].isspace(), result))
-            result = pd.DataFrame(result[:limit]).sort_values(by='count', ascending=True)
+            result = pd.DataFrame(result[:limit]).sort_values(by='count', ascending=False)
             if output_wc == 'Yes':
                 occurences = { keyword : count for keyword, count in zip(result["keyword"].tolist(), result["count"].tolist())}
                 wc = WordCloud().fit_words(occurences)
                 st.image(wc.to_array(), use_column_width=True, output_format='PNG')
             else:
                 result.rename(columns={'keyword': 'Keyword', 'count': 'Number of occurrences'}, inplace=True)
-                fig = px.bar(result, x='Number of occurrences', y='Keyword', orientation='h')
+                fig = px.bar(result[:limit], x='Number of occurrences', y='Keyword', orientation='h')
+                fig.update_yaxes(autorange='reversed')
                 st.write(fig)
 
     def distribution_number_comments_per_user(self):
@@ -72,21 +74,23 @@ class RedditAnalyzer:
         """
         Analyzes the news sources that are often used/cited on a specific subreddit
         """
-        limit = int(st.text_input("Limit", value="100"))
+        limit = int(st.text_input("Limit", value="30"))
         subreddit = st.selectbox(label='Subreddit', options=tuple(const.SUBREDDITS))
         if st.button('Show'):
-            result = pd.DataFrame(list(ap.reddit_frequently_used_news_sources(self.collection, subreddit))).sort_values(by='number_of_occurrences', ascending=True)
+            result = pd.DataFrame(list(ap.reddit_frequently_used_news_sources(self.collection, subreddit))).sort_values(by='number_of_occurrences', ascending=False)
             result.rename(columns={'domain': 'Domain', 'number_of_occurrences': 'Number of occurrences'}, inplace=True)
-            fig = px.bar(result, x='Number of occurrences', y='Domain', orientation='h')
+            fig = px.bar(result[:limit], x='Number of occurrences', y='Domain', orientation='h')
+            fig.update_yaxes(autorange='reversed')
             st.write(fig)
 
     def count_posts_per_user(self):
         """
         Analyzes the most active users / users with the most posts
         """
-        limit = int(st.text_input(label='Limit', value='100'))
+        limit = int(st.text_input(label='Limit', value='30'))
         if st.button('Show'):
-            result = pd.DataFrame(list(ap.reddit_count_posts_per_user(self.collection, limit))).sort_values(by='num_posts', ascending=True)
+            result = pd.DataFrame(list(ap.reddit_count_posts_per_user(self.collection, limit))).sort_values(by='num_posts', ascending=False)
             result.rename(columns={'_id': 'Username', 'num_posts': 'Number of posts'}, inplace=True)
             fig = px.bar(result, x='Number of posts', y='Username', orientation='h')
+            fig.update_yaxes(autorange='reversed')
             st.write(fig)
