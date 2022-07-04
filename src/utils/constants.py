f2a3a93c635438e454d66d2b899a7fb5fa56c130
tharealbasti <sebastian.strasser98@web.de>
2022-07-04 11:29:24 +0200

    Harmonize naming of analyses in the select box

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -7,31 +7,31 @@ DATA_SOURCE_COMBINED = 'Combined'
 ANALYSES_BY_DATA_SOURCE = {
     DATA_SOURCE_REDDIT: {
         'top_posts': 'Reddit Posts with highest score',
-        'most_controversial_posts': 'Most controversial Reddit Posts',
-        'subreddit_upvote_ratios': 'Average Upvote ratios per Subreddit',
+        'most_controversial_posts': 'Most controversial Reddit posts',
+        'subreddit_upvote_ratios': 'Average upvote ratios per subreddit',
         'comment_length_per_subreddit': 'Average comment length per subreddit',
         'keyword_per_subreddit': 'Most occurring keywords per subreddit',
         'distribution_number_comments_per_user': 'Distribution of number of comments per user',
         'distribution_number_posts_per_user': 'Distribution of number of posts per user',
         'frequently_used_news_sources': 'Frequently used news sources',
         'count_posts_per_user': 'Most active users',
-        'score_dist_by_hour': 'Average Post Score by hour'
+        'score_dist_by_hour': 'Average post score by hour'
     },
     DATA_SOURCE_TWITTER: {
         'most_common_hashtags': 'Common hashtags overall',
-        'high_interaction_hashtags': 'Hashtags with most Replies',
-        'most_liked_hashtags': 'Most liked Hashtags',
+        'high_interaction_hashtags': 'Hashtags with most replies',
+        'most_liked_hashtags': 'Most liked hashtags',
         'hashtags_per_trend': 'Common hashtags per trend',
         'create_hashtag_network_from_trend': 'Show hashtag network from trend',
         'recent_user_trends': 'Trends of recently created users',
         'bot_trends': 'Trends of (possible) bots',
         'longtime_user_trends': 'Trends of longtime users',
         'likes_by_membership_duration': 'Average likes aggregated by membership duration',
-        'followers_by_membership_duration': 'Average number of Followers aggregated by membership duration',
-        'replies_by_membership_duration': 'Average number of Replies aggregated by membership duration',
-        'retweets_by_membership_duration': 'Average number of Retweets aggregated by membership duration',
-        'quoted_by_membership_duration': 'Average number of Quotations aggregated by membership duration',
-        'verified_by_membership_duration': 'Percentage of verified Users aggregated by membership duration'
+        'followers_by_membership_duration': 'Average number of followers aggregated by membership duration',
+        'replies_by_membership_duration': 'Average number of replies aggregated by membership duration',
+        'retweets_by_membership_duration': 'Average number of retweets aggregated by membership duration',
+        'quoted_by_membership_duration': 'Average number of quotations aggregated by membership duration',
+        'verified_by_membership_duration': 'Percentage of verified users aggregated by membership duration'
     }, 
     DATA_SOURCE_RSS: {
         'publication_stats': 'Number of published articles for the most active sources',
@@ -43,8 +43,8 @@ ANALYSES_BY_DATA_SOURCE = {
         'published_dist_hour': 'Publication date distribution by hour',
         'published_dist_day': 'Publication date distribution by weekday',
         'headline_stats_per_feed_source': 'Headlines per news source',
-        'headline_relative_occurences': 'Influential Topics in Headlines per source',
-        'tags_per_source': 'Most common Keywords (tags) per news source'
+        'headline_relative_occurences': 'Influential topics in headlines per source',
+        'tags_per_source': 'Most common keywords (tags) per news source'
     }, 
     DATA_SOURCE_COMBINED: {
         'keyword_frequency_twitter': 'Occurrences of Twitter trends in news article',
